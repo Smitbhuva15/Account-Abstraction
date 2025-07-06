f45eb3cf8d59a33233d61168fb01953f96cccd12
Smitbhuva15 <itssmit123@gmail.com>
2025-07-06 17:05:54 +0000

    EntryPoint Contract

--- src/ethereum/ MinimalAccount.sol
+++ src/ethereum/ MinimalAccount.sol
@@ -7,23 +7,39 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
+import {IEntrypoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 
+contract MinimalAccount is IAccount, Ownable {
+    IEntrypoint public immutable i_entryPoint;
 
-contract  MinimalAccount is IAccount, Ownable {
-    constructor(address initialOwner) Ownable(initialOwner){}
+    error MinimalAccount__OnlyEntryPoint();
 
-       function validateUserOp(
+    constructor(address entryPoint) Ownable(msg.sender) {
+        i_entryPoint = IEntryPoint(entryPoint);
+    }
+
+    modifier onlyEntryPoint() {
+        if (msg.sender != address(i_entryPoint)) {
+            revert MinimalAccount__OnlyEntryPoint();
+        }
+        _;
+    }
+
+    function validateUserOp(
         PackedUserOperation calldata userOp,
         bytes32 userOpHash,
         uint256 missingAccountFunds
-    ) external returns (uint256 validationData){
-      validationData= _validateSignature(userOp,userOpHash);
-      _paypreFund(missingAccountFunds);
+    ) external onlyEntryPoint returns (uint256 validationData) {
+        validationData = _validateSignature(userOp, userOpHash);
+        _paypreFund(missingAccountFunds);
     }
 
     function _paypreFund(uint256 missingAccountFunds) internal {
         if (missingAccountFunds > 0) {
-           (bool success, ) =payable(msg.sender).call{value: missingAccountFunds,gas:type(uint256).max}("");
+            (bool success, ) = payable(msg.sender).call{
+                value: missingAccountFunds,
+                gas: type(uint256).max
+            }("");
             (success);
         }
     }
@@ -32,12 +48,11 @@ contract  MinimalAccount is IAccount, Ownable {
         PackedUserOperation calldata userOp,
         bytes32 userOpHash
     ) internal view returns (uint256 validationData) {
-      bytes32 hash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        bytes32 hash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
         address signer = ECDSA.recover(hash, userOp.signature);
         if (signer != owner()) {
             return SIG_VALIDATION_FAILED;
         }
         return SIG_VALIDATION_SUCCESS;
     }
-
 }
