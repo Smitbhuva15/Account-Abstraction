cd7e2391dc0b64b558f68b73bb53354a2605e50d
Smitbhuva15 <itssmit123@gmail.com>
2025-07-08 15:10:32 +0000

    signed op.. function

--- script/SendPackedUserOp.s.sol
+++ script/SendPackedUserOp.s.sol
@@ -4,21 +4,46 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 contract SendPackedUserOp is Script {
+
+    using MessageHashUtils for bytes32;
+
     function run() public {}
 
     function _generatesignedUserOperation(
         bytes memory callData,
-        address sender
+        HelperConfig.NetworkConfig memory config
     ) public view returns (PackedUserOperation memory) {
+
         // 1. Generate the unsigned data
-        uint256 nonce = vm.getNonce(sender) - 1;
+        uint256 nonce = vm.getNonce(config.account);
         PackedUserOperation memory userOp = _generateUnsignedUserOperation(
             callData,
-            sender,
+            config.account,
             nonce
         );
+ 
+         // 2. Get the userOp Hash
+         
+        bytes32 userOpHash=IEntryPoint(config.entryPoint).getUserOpHash(userOp);
+        bytes32 digest=userOpHash.toEthSignedMessageHash();
+
+         // 3. Sign it
+
+          uint8 v;
+        bytes32 r;
+        bytes32 s;
+        uint256 ANVIL_DEFAULT_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+        if (block.chainid == 31337) {
+            (v, r, s) = vm.sign(ANVIL_DEFAULT_KEY, digest);
+        } else {
+            (v, r, s) = vm.sign(config.account, digest);
+        }
+        userOp.signature = abi.encodePacked(r, s, v); 
+        return userOp;
     }
 
     function _generateUnsignedUserOperation(
