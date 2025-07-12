dd9a6070032217be6699f10097e87030a7cae725
Smitbhuva15 <itssmit123@gmail.com>
2025-07-12 10:27:39 +0000

    validate transaction test

--- test/zkSync/ZkMinimalAccountTest.t.sol
+++ test/zkSync/ZkMinimalAccountTest.t.sol
@@ -5,10 +5,16 @@ import {Test} from "forge-std/Test.sol";
 import {ZkMinimalAccount} from "../../src/zkSync/ZkMinimalAccount.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
+import {Transaction, MemoryTransactionHelper} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+
+import {BOOTLOADER_FORMAL_ADDRESS} from "lib/foundry-era-contracts/src/system-contracts/contracts/Constants.sol";
+import {ACCOUNT_VALIDATION_SUCCESS_MAGIC} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
+
 contract ZkMinimalAccountTest is Test {
     ZkMinimalAccount minimalAccount;
     ERC20Mock usdc;
     uint256 constant AMOUNT = 1e18;
+    bytes32 EMPTY_BYTES32 = bytes32(0);
 
     function setup() public {
         minimalAccount = new ZkMinimalAccount();
@@ -23,11 +29,76 @@ contract ZkMinimalAccountTest is Test {
             address(minimalAccount),
             AMOUNT
         );
-           Transaction memory transaction =
-            _createUnsignedTransaction(minimalAccount.owner(), 113, dest, value, functionData);
+        Transaction memory transaction = _createUnsignedTransaction(
+            minimalAccount.owner(),
+            113,
+            dest,
+            value,
+            functionData
+        );
+
+        vm.startPrank(minimalAccount.owner());
+        minimalAccount.executeTransaction(
+            EMPTY_BYTES32,
+            EMPTY_BYTES32,
+            transaction
+        );
+        vm.stopPrank();
+
+        assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
     }
 
-     function _createUnsignedTransaction(
+    function testZkValidateTransaction() public onlyZkSync {
+        // Arrange
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functionData = abi.encodeWithSelector(
+            ERC20Mock.mint.selector,
+            address(minimalAccount),
+            AMOUNT
+        );
+        Transaction memory transaction = _createUnsignedTransaction(
+            minimalAccount.owner(),
+            113,
+            dest,
+            value,
+            functionData
+        );
+
+        transaction = _signTransaction(transaction);
+
+        //Act
+        vm.stratPrank(BOOTLOADER_FORMAL_ADDRESS);
+        // only call --> BOOTLOADER_FORMAL_ADDRESS
+        bytes4 magic = minimalAccount.validateTransaction(
+            EMPTY_BYTES32,
+            EMPTY_BYTES32,
+            transaction
+        );
+        VM.stopPrank();
+
+        //Assert
+        assertEq(magic, ACCOUNT_VALIDATION_SUCCESS_MAGIC);
+    }
+
+    function _signTransaction(
+        Transaction memory transaction
+    ) internal view returns (Transaction memory) {
+        bytes32 unsignedTransactionHash = MemoryTransactionHelper.encodeHash(
+            transaction
+        );
+
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+        uint256 ANVIL_DEFAULT_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+        (v, r, s) = vm.sign(ANVIL_DEFAULT_KEY, unsignedTransactionHash);
+        Transaction memory signedTransaction = transaction;
+        signedTransaction.signature = abi.encodePacked(r, s, v);
+        return signedTransaction;
+    }
+
+    function _createUnsignedTransaction(
         address from,
         uint8 transactionType,
         address to,
@@ -36,23 +107,24 @@ contract ZkMinimalAccountTest is Test {
     ) internal view returns (Transaction memory) {
         uint256 nonce = vm.getNonce(address(minimalAccount));
         bytes32[] memory factoryDeps = new bytes32[](0);
-        return Transaction({
-            txType: transactionType,
-            from: uint256(uint160(from)),
-            to: uint256(uint160(to)),
-            gasLimit: 16777216,
-            gasPerPubdataByteLimit: 16777216,
-            maxFeePerGas: 16777216,
-            maxPriorityFeePerGas: 16777216,
-            paymaster: 0,
-            nonce: nonce,
-            value: value,
-            reserved: [uint256(0), uint256(0), uint256(0), uint256(0)],
-            data: data,
-            signature: hex"",
-            factoryDeps: factoryDeps,
-            paymasterInput: hex"",
-            reservedDynamic: hex""
-        });
+        return
+            Transaction({
+                txType: transactionType,
+                from: uint256(uint160(from)),
+                to: uint256(uint160(to)),
+                gasLimit: 16777216,
+                gasPerPubdataByteLimit: 16777216,
+                maxFeePerGas: 16777216,
+                maxPriorityFeePerGas: 16777216,
+                paymaster: 0,
+                nonce: nonce,
+                value: value,
+                reserved: [uint256(0), uint256(0), uint256(0), uint256(0)],
+                data: data,
+                signature: hex"",
+                factoryDeps: factoryDeps,
+                paymasterInput: hex"",
+                reservedDynamic: hex""
+            });
     }
 }
